69e93c0a08b982cd38dcca3a3e003f9639a5a4fc
= <=>
2023-08-21 19:50:38 -0400

    added styles and small tweaks to HTML and index.js

--- src/index.js
+++ src/index.js
@@ -3,19 +3,21 @@ import getGames from './rawgCall.js'
 async function renderGames(params){
     const rawGamesList = await getGames(params)
     const gamesList = rawGamesList.filter(game=>game.background_image!=null)
-    const content = document.querySelector('.content')
+    const gamesSection = document.querySelector('.games')
 
     gamesList.forEach(game=>{
         let block = document.createElement('article')
+        block.setAttribute('class', 'card')
         block.appendChild(getGameImageHTML(game))
         block.appendChild(getGameTitleHTML(game))
-        content.appendChild(block)
+        gamesSection.appendChild(block)
     })
 }
 
 function getGameImageHTML(game){
     let gameImage = document.createElement('img')
         gameImage.setAttribute('src', game.background_image)
+        gameImage.setAttribute('class', 'game-image')
         gameImage.setAttribute('width', '100px')
         gameImage.setAttribute('height', '100px')
     return gameImage
@@ -23,8 +25,11 @@ function getGameImageHTML(game){
 
 function getGameTitleHTML(game){
     let gameTitle = document.createElement('p')
+    gameTitle.setAttribute('class', 'game-title')
     gameTitle.textContent = game.name
     return gameTitle
 }
 
-renderGames('ordering=released&page=1')
+for(let i = 0 ; i < 5 ; i++){
+    renderGames(`ordering=released&page=${i}`)
+}
