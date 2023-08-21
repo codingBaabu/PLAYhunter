4595f8ab1aa8582e5dde40b1636f18eaafb84184
= <=>
2023-08-21 11:42:34 -0400

    rendered games and titles to screen

--- src/index.js
+++ src/index.js
@@ -1,8 +1,30 @@
 import getGames from './rawgCall.js'
 
 async function renderGames(params){
-    const gamesList = await getGames(params)
-    console.log(gamesList)
+    const rawGamesList = await getGames(params)
+    const gamesList = rawGamesList.filter(game=>game.background_image!=null)
+    const content = document.querySelector('.content')
+
+    gamesList.forEach(game=>{
+        let block = document.createElement('article')
+        block.appendChild(getGameImageHTML(game))
+        block.appendChild(getGameTitleHTML(game))
+        content.appendChild(block)
+    })
+}
+
+function getGameImageHTML(game){
+    let gameImage = document.createElement('img')
+        gameImage.setAttribute('src', game.background_image)
+        gameImage.setAttribute('width', '100px')
+        gameImage.setAttribute('height', '100px')
+    return gameImage
+}
+
+function getGameTitleHTML(game){
+    let gameTitle = document.createElement('p')
+    gameTitle.textContent = game.name
+    return gameTitle
 }
 
 renderGames('ordering=released&page=1')
