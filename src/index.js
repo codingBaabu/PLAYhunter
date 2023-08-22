aa009f37b75c59b15e4d7aa18cc4aee900b5e78f
= <=>
2023-08-22 19:05:40 -0400

    added infinite scroll and masonry layout

--- src/index.js
+++ src/index.js
@@ -1,17 +1,63 @@
 import getGames from './rawgCall.js'
 
-async function renderGames(params){
+let currentPage=1
+let colcade
+
+window.addEventListener('scroll', addMoreGames)
+
+document.addEventListener('DOMContentLoaded', function () {
+    var grid = document.querySelector('.games'); 
+    colcade = new Colcade(grid, {
+        columns: '.grid-col',
+        items: '.card'
+  });
+})
+
+function addMoreGames(){
+    if(window.scrollY+window.innerHeight>=document.documentElement.scrollHeight){
+        const svg = document.getElementById('linear')
+        svg.style.visibility = 'visible'
+        
+        document.querySelector('.loading-more-content').style.display = 'flex'
+        
+        for(let i = currentPage ; i < currentPage + 3; i++){
+            renderGames(`ordering=released&page=${i}`, i-currentPage+1)
+        }
+        currentPage+=3
+    }
+}
+
+async function renderGames(params, cPage){
     const rawGamesList = await getGames(params)
     const gamesList = rawGamesList.filter(game=>game.background_image!=null)
-    const gamesSection = document.querySelector('.games')
 
     gamesList.forEach(game=>{
-        let block = document.createElement('article')
-        block.setAttribute('class', 'card')
-        block.appendChild(getGameImageHTML(game))
-        block.appendChild(getGameTitleHTML(game))
-        gamesSection.appendChild(block)
+            let block = document.createElement('article')
+            block.setAttribute('class', 'card')
+            block.appendChild(getGameImageHTML(game))
+            block.appendChild(getGameTitleHTML(game))
+
+            let smallestColumn = getSmallestColumn()
+            smallestColumn.appendChild(block)
     })
+
+    document.getElementById('linear').style.visibility = 'hidden'
+    document.getElementById('spinner').style.visibility = 'hidden'
+    document.querySelector('.loading-initial-content').style.display = 'none'
+}
+
+function getSmallestColumn(){
+    let columns = document.querySelectorAll('.grid-col')
+    let smallestColumn = columns[0]
+
+    columns.forEach(column=>{
+        if(column.offsetHeight < smallestColumn.offsetHeight){
+            smallestColumn = column           
+        }
+    })
+
+
+    return smallestColumn
 }
 
 function getGameImageHTML(game){
@@ -30,6 +76,6 @@ function getGameTitleHTML(game){
     return gameTitle
 }
 
-for(let i = 0 ; i < 5 ; i++){
-    renderGames(`ordering=released&page=${i}`)
+for(currentPage ; currentPage < 4 ; currentPage++){
+    renderGames(`ordering=released&page=${currentPage}`, currentPage)
 }
